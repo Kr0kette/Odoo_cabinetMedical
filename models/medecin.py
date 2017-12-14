3972add46b34ff4119b31106857fa1a5305301a9
De Backer Christophe <39871@etu.he2b.be>
2017-12-14 10:03:42 +0100

    héritage medecin => res.users

--- models/medecin.py
+++ models/medecin.py
@@ -5,7 +5,3 @@ from odoo import fields, models
 class Medecin(models.Model):
     _inherit = 'res.users'
 
-    # Add a new column to the res.partner model, by default partners are not
-    # medecins
-    medecin = fields.Boolean("Medecin", default=False)
-
