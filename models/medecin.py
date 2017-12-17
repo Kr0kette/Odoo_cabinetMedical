519c4326204f57a8b5eaebb2d4ecaaa72824a336
De Backer Christophe <39871@etu.he2b.be>
2017-12-17 19:39:59 +0100

    Adds multiple 'Etablissement' possibilities

--- models/medecin.py
+++ models/medecin.py
@@ -4,7 +4,7 @@ from odoo import fields, models
 
 class Medecin(models.Model):
     _inherit = 'res.users'
-    etablissement_id = fields.Many2one('cabinetmedical.etablissement',
+    etablissement_id = fields.Many2many('cabinetmedical.etablissement',
                                        ondelete='cascade', string="Etablissement")
 
     specialite_id=fields.Many2one('cabinetmedical.specialite',
